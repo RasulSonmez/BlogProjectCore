2952881dd14b9205e91fd2cc265cdbad7a75ba48
rasul sönmez <barbaros743@outlook.com>
2023-06-20 19:13:47 +0300

    kljklkj45645654

--- BlogProjectCore/Controllers/AboutController.cs
+++ BlogProjectCore/Controllers/AboutController.cs
@@ -1,17 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFrameWork;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BlogProjectCore.Controllers
 {
+   
     public class AboutController : Controller
     {
+        AboutManager abm = new AboutManager(new EfAboutRepository());
+
         public IActionResult Index()
         {
-            return View();
+            var values = abm.GetList();
+            return View(values);
         }
 
         public PartialViewResult SocialMediaAbout()
         {
-
+           
             return PartialView();    
         }
     }
---
+++ BlogProjectCore/Controllers/ContactController.cs
@@ -0,0 +1,45 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFrameWork;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace BlogProjectCore.Controllers
+{
+    public class ContactController : Controller
+    {
+        ContactManager cm = new ContactManager(new EfContactRepository());
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Contact contact)
+        {
+            ContactValidator cv = new ContactValidator();
+            ValidationResult result = cv.Validate(contact);
+
+            if (result.IsValid)
+            {
+                contact.ContactDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+                contact.ContactStatus = true;
+                cm.ContactAdd(contact);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+                return View();
+            }
+
+        }
+    }
+}
